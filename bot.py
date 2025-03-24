9ca899144b0e6ceb2f06c973e5ac446f0aed2121
luizh088 <85356590+luizh088@users.noreply.github.com>
2025-03-23 22:54:46 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -4,15 +4,17 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("BOT_TOKEN:", TOKEN)
-print("CHAT_ID:", CHAT_ID)
+print(f"TOKEN: {TOKEN}")
+print(f"CHAT_ID: {CHAT_ID}")
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 payload = {
-    "chat_id": CHAT_ID,
-    "text": "Teste de envio pelo bot via Railway"
+    "chat_id": str(CHAT_ID),
+    "text": "Teste de envio",
+    "parse_mode": "Markdown"
 }
 
+print("== Enviando mensagem de teste ==")
 r = requests.post(url, data=payload)
 print("Status:", r.status_code)
 print("Resposta:", r.text)
